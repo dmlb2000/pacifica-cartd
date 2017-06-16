ad96815a6a89b66ad33a369abfed10a1286bf6cb
David Brown <dmlb2000@gmail.com>
2017-06-15 19:40:49 -0700

    add the requirements.txt parsing logic to setup.py (#16)

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 """Setup and install the cart."""
+from pip.req import parse_requirements
 from setuptools import setup
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
 
 setup(name='PacificaCartd',
       version='1.0',
@@ -8,4 +12,5 @@ setup(name='PacificaCartd',
       author='David Brown',
       author_email='david.brown@pnnl.gov',
       packages=['cart'],
-      scripts=['CartServer.py', 'DatabaseCreate.py'])
+      scripts=['CartServer.py', 'DatabaseCreate.py'],
+      install_requires=[str(ir.req) for ir in INSTALL_REQS])
