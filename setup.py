afd021264d10699301bb986be7798c818003e230
David Brown <dmlb2000@gmail.com>
2017-12-19 16:18:37 -0800

    add setuptools scm to handle version (#27)
    * add setuptools scm to handle version
    
    * setup requirements

--- setup.py
+++ setup.py
@@ -7,14 +7,17 @@ from setuptools import setup
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
 
-setup(name='PacificaCartd',
-      version='1.0',
-      description='Pacifica Cartd',
-      author='David Brown',
-      author_email='david.brown@pnnl.gov',
-      packages=['cart'],
-      scripts=['CartServer.py', 'DatabaseCreate.py'],
-      entry_point={
-          'console_scripts': ['CartServer=cart.__main__:main']
-      },
-      install_requires=[str(ir.req) for ir in INSTALL_REQS])
+setup(
+    name='PacificaCartd',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='Pacifica Cartd',
+    author='David Brown',
+    author_email='david.brown@pnnl.gov',
+    packages=['cart'],
+    scripts=['CartServer.py', 'DatabaseCreate.py'],
+    entry_point={
+        'console_scripts': ['CartServer=cart.__main__:main']
+    },
+    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+)
